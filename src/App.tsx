376a721ffbb9efc8bb0fc67d7e98a6b08765a806
Ivan Mikerin <vmik7@yandex.ru>
2023-03-03 17:52:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import styles from './App.module.css';
+
+export const App: FC = function App() {
+    return <div className={styles.wrapper}>Hello World!</div>;
+};
+
+export default App;
